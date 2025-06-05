5d5a8e40173c22058aa8b05ef542075696227ab0
Pablo Prato <p.prato@alumno.etec.um.edu.ar>
2025-06-05 09:51:17 -0300

    hola profe, perdon qu te lo envie tan tarde, me estaba dando unos problemas que no sabia como solucionarlos,nos vemos el viernes

--- app/db.py
+++
@@ -1,8 +0,0 @@
-from tortoise import Tortoise
-
-async def init():
-    await Tortoise.init(
-        db_url='sqlite://db.sqlite3',
-        modules={'models': ['app.models']}
-    )
-    await Tortoise.generate_schemas()
